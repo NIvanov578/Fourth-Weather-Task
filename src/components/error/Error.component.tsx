f08816453247ca0167e69eb3d2f4ffb3b39fad79
Nikola Ivanov <nikola.ivanov@theopentag.com>
2022-09-13 11:08:35 +0300

    remove components

--- src/components/error/Error.component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ErrorProps } from './Error.model';
-import { Styled } from './Error.styled';
-
-const Error: React.FC<ErrorProps > =  ({ error })=> (
-    <Styled.ErrorMessage>{error}</Styled.ErrorMessage>
-)
-
-export default Error;
--- src/components/spinner/Spinner.model.ts
+++
@@ -1,3 +0,0 @@
-export type SpinnerProps = Readonly<{
-    isLoading: boolean;
-}>
